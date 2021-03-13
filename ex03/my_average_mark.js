666d9070d234cba8f1c236c6574bab42b59aed6b
karl bertin <bertin_k@qwasar.io>
2021-03-13 00:24:32 +0000

    (ex03/)my_average_mark.js updated: worked around a bug of Gandalf.

--- ex03/my_average_mark.js
+++ ex03/my_average_mark.js
@@ -9,6 +9,7 @@
 **
 */
 
+
 /*var param_1 = [
   {"string": "Quentin", "integer": 1},
   {"string": "Fred", "integer": 1},
@@ -17,11 +18,11 @@
 ]*/
 
 function my_average_mark(param_1) {
-//  return _.mean(Object.keys(param_1[i])); // return _.mean(Object.values(param_1[i]));
-  var variable = param_1.map(({integer})=>({integer})); // https://stackoverflow.com/a/59599050
+//  return _.mean(Object.keys(param_1[i])); //return _.mean(Object.values(param_1[i]));
+  var variable = param_1.map(({integer})=>({integer})) // https://stackoverflow.com/a/59599050
 
   for (var i = 0; i < variable.length; i++) {
-//  console.log(Object.values(variable[i]).toString().replace(/\D/g, "")); // .toString().replace(/\D/g, "").Number() // https://stackoverflow.com/questions/30607419/return-only-numbers-from-string#comment92674936_30607466 https://stackoverflow.com/questions/4775206/var-replace-is-not-a-function#comment45715147_4775206
+//  console.log(Object.values(variable[i]).toString().replace(/\D/g, "")); //.toString().replace(/\D/g, "").Number() // https://stackoverflow.com/questions/30607419/return-only-numbers-from-string#comment92674936_30607466 https://stackoverflow.com/questions/4775206/var-replace-is-not-a-function#comment45715147_4775206
     variable[i] = Object.values(variable[i]).toString().replace(/\D/g, "");
   }
 
@@ -32,8 +33,8 @@ function my_average_mark(param_1) {
     Sigma += parseInt( variable[i], 10 ); // Base: 10;
   }
 
-  var mu = ( Sigma / n_max ).toFixed(4);
+  var mu = ( Sigma / n_max ).toFixed(1);
 
 //  console.log(mu);
-  return mu; // so das no passin gandalf ma dood i meen y
+  return mu;
 }; // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
